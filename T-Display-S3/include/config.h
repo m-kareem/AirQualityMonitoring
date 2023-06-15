1b8d89988632f705b456ba2ed54a4cbf6409b4f6
mkareem <mohammad.kareem@cern.ch>
2023-06-15 06:34:51 -0400

    git fix

--- T-Display-S3/include/config.h
+++ T-Display-S3/include/config.h
@@ -12,8 +12,8 @@ const char *sensor_type="SHT35"; // SHT35, SGP30, or Multi
 #define device_pref "SC-" // Give the device a name!
 
 // for OTA drive
-#define firmware_version "v@1.0.0.0s"
-#define ProductKey "YOUR_PRODUCT_KEY"
+#define firmware_version "v@1.0.1.9"
+#define ProductKey "5d0d1086-4c6b-4053-9932-5e7ff0b1acba"
 #define update_timeTick 3600
 
 // I2C pins in lilygo T-Display-S3 board
@@ -29,26 +29,23 @@ const char *sensor_type="SHT35"; // SHT35, SGP30, or Multi
 int _device_number = 99;
 int _power_saving = 0;
 int _MQTT_PUBLISH_DELAY_ms = 5000;
-//const char _MQTT_SERVER = "petra.phys.yorku.ca";
-//const char _EAP_ssid = "AirYorkPLUS";
 
 // ============ NETWORK setting =======================================
 
 const bool EAP_wifi= false; // change according to the WIFI type [true for enterprise networks, false for normal ]
 
 //------ ENT Wifi setting (e.g. eduroam)-------------------
-#define EAP_ANONYMOUS_IDENTITY "user@domain.ca"
-#define EAP_IDENTITY "user@domain.ca"
-#define EAP_PASSWORD "your_pass" // hardcode your wifi password here. It may not a good idea if you want to share your code
-const char* EAP_ssid = "your_network_ssid"; //  network's SSID
+#define EAP_ANONYMOUS_IDENTITY "mkareem@yorku.ca"
+#define EAP_IDENTITY "mkareem@yorku.ca"
+#define EAP_PASSWORD "sghl.sghlY4" // hardcode your wifi password here
+const char* EAP_ssid = "AirYorkPLUS"; //  network's SSID
 
 //------ non-ENT Wifi setting-------------------
 const char *ssid = "AirYorkGUEST";
 //const char *password = none;
 
-
 // ============ MQTT setting =======================================
-const char *MQTT_SERVER = "Your_mqtt_server"; // the server that hosts the mqtt
+const char *MQTT_SERVER = "petra.phys.yorku.ca";
 const int MQTT_PORT = 1883;
 
 //#define MQTT_PUBLISH_DELAY 5000
